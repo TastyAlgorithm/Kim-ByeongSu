d8bce32e74a41aee42836a1b9e1cf9fe9ad354f6
TastyAlgorithm <76274621+TastyAlgorithm@users.noreply.github.com>
2021-01-04 22:36:45 +0900

    Update BRACKETS2.cpp

--- 20210105/BRACKETS2.cpp
+++ 20210105/BRACKETS2.cpp
@@ -21,6 +21,7 @@ int main() {
 		bool check = true;
 		stack<char> stk;
 		for (int j = 0; j < input.size(); j++) {
+			// push open bracket in stack
 			if (input[j] == '(' || input[j] == '{' || input[j] == '[') {
 				stk.push(input[j]);
 			}
@@ -50,6 +51,10 @@ int main() {
 			}
 		}
 
+		/* 
+		모든 인풋에 대한 처리를 수행했는데 스택이 비어있지 않음 -> open bracket 이 더 많은 경우에 해당
+		check 변수가 false 인 경우 -> close bracket 을 처리할 때, 짝이 맞지 않았거나 스택이 비어있는 경우에 
+		*/
 		if (!stk.empty() || !check) {
 			answer[i] = "NO";
 		}
