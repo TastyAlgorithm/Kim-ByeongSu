78ae1d5668dc782c5d97e131bbe9d3a9e95076cd
TastyAlgorithm <76274621+TastyAlgorithm@users.noreply.github.com>
2021-01-19 13:18:45 +0900

    Update FENCE.cpp

--- 20210112/FENCE.cpp
+++ 20210112/FENCE.cpp
@@ -31,7 +31,12 @@ int main(void) {
 			fence.push_back(temp);
 		}
 		fence.push_back(-1);
-
+		/*
+		23, 33 번째 줄에서 -1 을 push 해주는 이유?
+		input 으로 7 1 5 9 6 7 3 라는 숫자들이 주어졌을 때, -1 을 맨 처음과 맨 마지막에 넣어주면 
+		getLeft() 함수와 getRight() 함수의 for loop 에서 index 범위를 벗어났는지 따로 체크하지 않아도 되기 때문.
+		*/
+		
 		answer[i] = FENCE();
 	}
 
