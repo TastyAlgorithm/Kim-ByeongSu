e9399266346f732d1c0bebb691c9e9741ec874b9
TastyAlgorithm <76274621+TastyAlgorithm@users.noreply.github.com>
2021-02-10 12:09:44 +0900

    Create 9372_상근이의여행.cpp

---
+++ 20210216/9372_상근이의여행.cpp
@@ -0,0 +1,35 @@
+/*
+  url: https://www.acmicpc.net/problem/9372
+*/
+
+#include<iostream>
+#include<vector>
+
+using namespace std;
+
+int main(void) {
+	ios_base::sync_with_stdio(0);
+	cin.tie(0);
+	
+	int T;
+	cin >> T;
+	vector<int> answer(T, 0);
+
+	for (int i = 0; i < T; i++) {
+		int N, M;		// 국가의 수, 비행기의 수
+		cin >> N >> M;
+
+		for (int j = 0; j < M; j++) {
+			int a, b;
+			cin >> a >> b;
+		}
+
+		answer[i] = N - 1;
+	}
+
+	for (int output : answer) {
+		cout << output << "\n";
+	}
+
+	return 0;
+}
